3664d84704a3cd02dd5e7a1c934570c027e1745b
Andrew Munro <synestry@gmail.com>
2023-03-28 19:22:26 +0000

    Disable HMR on server

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import mix from 'vite-plugin-mix';
 
 export default defineConfig({
+	server: {
+		hmr: !process.env.DISABLE_HMR,
+	},
 	plugins: [
 		mix.default({
 			handler: './src/server.ts'
